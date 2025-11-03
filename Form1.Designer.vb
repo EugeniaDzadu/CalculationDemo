657d8eba77c64b8c0fa0ee6dca6c1156f1f8e931
EugeniaDzadu <jeeniaace@gmail.com>
2025-11-03 14:05:55 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtFirstNum = New TextBox()
+        txtSecondNum = New TextBox()
+        lblFirstNum = New Label()
+        lblSecondNum = New Label()
+        lblResult = New Label()
+        btnAdd = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        btnCalculate = New Button()
+        btnDivide = New Button()
+        btnMultiply = New Button()
+        SuspendLayout()
+        ' 
+        ' txtFirstNum
+        ' 
+        txtFirstNum.Location = New Point(318, 48)
+        txtFirstNum.Name = "txtFirstNum"
+        txtFirstNum.Size = New Size(314, 27)
+        txtFirstNum.TabIndex = 0
+        txtFirstNum.Text = "0"
+        ' 
+        ' txtSecondNum
+        ' 
+        txtSecondNum.Location = New Point(318, 138)
+        txtSecondNum.Name = "txtSecondNum"
+        txtSecondNum.Size = New Size(314, 27)
+        txtSecondNum.TabIndex = 1
+        ' 
+        ' lblFirstNum
+        ' 
+        lblFirstNum.AutoSize = True
+        lblFirstNum.Location = New Point(175, 55)
+        lblFirstNum.Name = "lblFirstNum"
+        lblFirstNum.Size = New Size(94, 20)
+        lblFirstNum.TabIndex = 2
+        lblFirstNum.Text = "First Number"
+        ' 
+        ' lblSecondNum
+        ' 
+        lblSecondNum.AutoSize = True
+        lblSecondNum.Location = New Point(175, 138)
+        lblSecondNum.Name = "lblSecondNum"
+        lblSecondNum.Size = New Size(116, 20)
+        lblSecondNum.TabIndex = 3
+        lblSecondNum.Text = "Second Number"
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.BackColor = SystemColors.ActiveCaption
+        lblResult.Location = New Point(175, 224)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(49, 20)
+        lblResult.TabIndex = 4
+        lblResult.Text = "Result"
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(238, 289)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(94, 29)
+        btnAdd.TabIndex = 5
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(549, 289)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(94, 29)
+        btnClear.TabIndex = 6
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(549, 372)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(94, 29)
+        btnExit.TabIndex = 7
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Location = New Point(238, 372)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(94, 29)
+        btnCalculate.TabIndex = 8
+        btnCalculate.Text = "Substract"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Location = New Point(388, 372)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(94, 29)
+        btnDivide.TabIndex = 9
+        btnDivide.Text = "Divide"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Location = New Point(388, 289)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(94, 29)
+        btnMultiply.TabIndex = 10
+        btnMultiply.Text = "Multiply"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 508)
+        Controls.Add(btnMultiply)
+        Controls.Add(btnDivide)
+        Controls.Add(btnCalculate)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnAdd)
+        Controls.Add(lblResult)
+        Controls.Add(lblSecondNum)
+        Controls.Add(lblFirstNum)
+        Controls.Add(txtSecondNum)
+        Controls.Add(txtFirstNum)
+        Name = "Form1"
+        Text = "Form1"
+        TopMost = True
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtFirstNum As TextBox
+    Friend WithEvents txtSecondNum As TextBox
+    Friend WithEvents lblFirstNum As Label
+    Friend WithEvents lblSecondNum As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnMultiply As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Me.txtFirstNum.Text = " "
+        Me.txtSecondNum.Text = " "
+        Me.lblResult.Text = " "
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Me.txtFirstNum.Text = 20
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        lblResult.Text = CInt(txtFirstNum.Text) + CInt(txtSecondNum.Text)
+
+
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        lblResult.Text = CInt(txtFirstNum.Text) * CInt(txtSecondNum.Text)
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        lblResult.Text = CInt(txtFirstNum.Text) - CInt(txtSecondNum.Text)
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        lblResult.Text = CInt(txtFirstNum.Text) / CInt(txtSecondNum.Text)
+    End Sub
+End Class
